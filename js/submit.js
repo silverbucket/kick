6229c721d74ce6288d982c3955f550ecc6b8e182
Nick Jennings <nick@silverbucket.net>
2012-08-31 14:40:36 +0200

    per-page javascript files

---
+++ js/submit.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+    $('a#generate_code').click(function() {
+        console.log('intercepted generate code');
+        return false;
+    });
+
+    $("#submit_kick_form").validate({
+        //set the rules for the field names
+        rules: {
+            kick_title: {
+                required: false
+            },
+            kick_url: {
+                required: true,
+                url: true
+            },
+            kick_code: {
+                required: true,
+                minlength: 6
+            },
+        },
+        //set messages to appear inline
+        messages: {
+            kick_title: "",
+            kick_url: "",
+            kick_code: ""
+        },
+        highlight: function(label) {
+            $(label).css('border-color', 'rgba(246, 20, 29, .3)');
+        },
+        success: function(label) {
+            $(label).css('border-color', 'rgba(20, 246, 20, .3)');
+        },
+        submitHandler: function(form) {
+            console.log('form submittion passed validation');
+            return false;
+        }
+    });
+    
+});
